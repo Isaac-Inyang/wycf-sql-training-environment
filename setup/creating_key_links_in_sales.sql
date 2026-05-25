1998064f2503a4e90e84cad715c84bd712b02133
Isaac Inyang <ukoisaac5@gmail.com>
2026-05-25 17:17:09 +0100

    Update creating_key_links_in_sales.sql

--- setup/creating_key_links_in_sales.sql
+++ setup/creating_key_links_in_sales.sql
@@ -1,21 +1,24 @@
--- SELECT sale_key::int, COUNT(*)
--- FROM sales 
--- GROUP BY sale_key::int
--- HAVING COUNT(*) > 1
--- LIMIT 20;
+-- checking duplicates
+SELECT sale_key::int, COUNT(*)
+FROM sales 
+GROUP BY sale_key::int
+HAVING COUNT(*) > 1
+LIMIT 20;
 
--- DELETE FROM sales
--- 	WHERE customer_key::int = 0 OR bill_to_customer_key::int =0;
+-- removing irrelevant rows
+DELETE FROM sales
+	WHERE customer_key::int = 0 OR bill_to_customer_key::int =0;
 
--- ALTER TABLE sales
--- 	ALTER COLUMN sale_key TYPE INTEGER USING sale_key::integer,
--- 	ALTER COLUMN city_key TYPE INTEGER USING city_key::integer,
--- 	ALTER COLUMN stock_item_key TYPE INTEGER USING stock_item_key::integer,
--- 	ALTER COLUMN salesperson_key TYPE INTEGER USING salesperson_key::integer,
--- 	ALTER COLUMN wwi_invoice_id TYPE INTEGER USING wwi_invoice_id::integer,
---	ALTER COLUMN customer_key TYPE INTEGER USING customer_key::integer,
---	ALTER COLUMN bill_to_customer_key TYPE INTEGER USING bill_to_customer_key::integer,
--- 	ADD PRIMARY KEY (sale_key);
+-- correcting datatypes and creating neccessary links
+ALTER TABLE sales
+	ALTER COLUMN sale_key TYPE INTEGER USING sale_key::integer,
+	ALTER COLUMN city_key TYPE INTEGER USING city_key::integer,
+	ALTER COLUMN stock_item_key TYPE INTEGER USING stock_item_key::integer,
+	ALTER COLUMN salesperson_key TYPE INTEGER USING salesperson_key::integer,
+	ALTER COLUMN wwi_invoice_id TYPE INTEGER USING wwi_invoice_id::integer,
+	ALTER COLUMN customer_key TYPE INTEGER USING customer_key::integer,
+	ALTER COLUMN bill_to_customer_key TYPE INTEGER USING bill_to_customer_key::integer,
+	ADD PRIMARY KEY (sale_key);
 
 -- creating foreign keys
 ALTER TABLE sales
