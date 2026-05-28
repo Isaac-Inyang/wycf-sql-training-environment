48eeb5003d38dd21fa5f88014ac59da39e42ac38
Isaac Inyang <ukoisaac5@gmail.com>
2026-05-29 00:40:14 +0100

    Delete setup/creating_links_in_city_table.sql

--- setup/creating_links_in_city_table.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM city;
-
--- changing datatype and adding primary key
-ALTER TABLE city
-	ALTER COLUMN city_key TYPE INTEGER USING city_key::integer,
-	ADD PRIMARY KEY (city_key);
-
-SELECT * FROM city;
